e7e3994ffcb47c997c5e1094fa2d5427386ffe53
张博 <bobobo@zhangbos-MacBook-Pro.local>
2021-09-20 12:43:42 +0800

    Separate from pin.

--- pin/dev.bash
+++
@@ -1,3 +0,0 @@
-ENGINE_PIN=$(cd "$(dirname "$0")"; pwd)
-export PYTHONPATH=$PYTHONPATH:${ENGINE_PIN}
-echo "Add Python Path: "${ENGINE_PIN}
--- pin/tests/token_mocker.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-#BoBoBo#
-
-import json
-
-
-def get_auth_str(token):
-    return json.dumps({'uid': 100})
